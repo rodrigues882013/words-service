82ca0079fd546e0ba8d24dd49c5af96475cf4c08
frodrigues <frodrigues@amtera.com.br>
2017-02-22 22:40:06 -0300

    -- change recursive aproach to interative and adding more tests

--- src/main/java/com/words/services/UtilsService.java
+++ src/main/java/com/words/services/UtilsService.java
@@ -2,13 +2,44 @@ package com.words.services;
 
 import com.words.models.Word;
 
+
 public class UtilsService {
 
-    public static Integer calculeDistance(Word w1, Word w2){
+    public static Integer minimum(Integer v1, Integer v2, Integer v3){
+        return Math.min(Math.min(v1, v2), v3);
+    }
+
+    public static Integer distance(Word w1, Word w2){
+
         if (w1 == null || w2 == null) throw new NullPointerException("Word 1 or Word 2 have null value");
-        if (w1.getWord().equals("") && !w2.getWord().equals("") ||
-                w2.getWord().equals("") && !w1.getWord().equals("")) return 0;
-        if (w1.equals(w2)) return 0;
-        return -1;
+
+        String s1 = w1.getWord().toLowerCase();
+        String s2 = w2.getWord().toLowerCase();
+        Integer[][] result = new Integer[s1.length()+1][s2.length()+1];
+        Integer cost = 0;
+
+
+        if (s1.equals("") && !s2.equals("") || s2.equals("") && !s1.equals("") || w1.equals(w2)) return 0;
+
+        for (int i=0; i <= s1.length(); i++) result[i][0] = i;
+        for (int j=0; j <= s2.length(); j++) result[0][j] = j;
+
+        for (int i=1; i <= s1.length(); i++){
+
+            for (int j=1; j <= s2.length(); j++){
+
+                if (s1.charAt(i-1) == s2.charAt(j-1)) cost = 0;
+                else cost = 1;
+
+                result[i][j] = minimum(result[i-1][j] + 1,
+                                       result[i][j-1] + 1,
+                                       result[i-1][j-1] + cost);
+
+            }
+        }
+
+
+
+        return result[s1.length()][s2.length()];
     }
 }
--- src/test/java/com/words/services/UtilsServiceTest.java
+++ src/test/java/com/words/services/UtilsServiceTest.java
@@ -10,11 +10,13 @@ import static org.junit.Assert.assertTrue;
 public class UtilsServiceTest {
 
     private Word w1, w2;
+    private Integer distance;
 
     @Before
     public void setUp(){
         w1 = new Word();
         w2 = new Word();
+
 
     }
     @Test
@@ -22,7 +24,7 @@ public class UtilsServiceTest {
         w1.setWord("ball");
         w2.setWord("ball");
 
-        Integer distance = UtilsService.calculeDistance(w1, w2);
+        distance = UtilsService.distance(w1, w2);
         assertTrue("Two equal words have to be 0", distance == 0);
 
 
@@ -33,20 +35,34 @@ public class UtilsServiceTest {
         w1.setWord("");
         w2.setWord("ball");
 
-        Integer distance = UtilsService.calculeDistance(w1, w2);
+        distance = UtilsService.distance(w1, w2);
         assertTrue("Distance between '' and 'ball' have to be 0", distance == 0);
 
         w1.setWord("ball");
         w2.setWord("");
 
-        distance = UtilsService.calculeDistance(w1, w2);
+        distance = UtilsService.distance(w1, w2);
         assertTrue("Distance between 'ball' and '' have to be 0", distance == 0);
     }
 
     @Test(expected = NullPointerException.class)
     public void nullWordsTeste(){
-        Integer distance = UtilsService.calculeDistance(null, null);
+        distance = UtilsService.distance(null, null);
     }
 
+    @Test
+    public void calculeDistanceBetweenTwoWordsTest(){
+        w1.setWord("abacate");
+        w2.setWord("banana");
+
+        distance = UtilsService.distance(w1, w2);
+        assertTrue("Distance between 'abacate' and 'banana' have to be diferente from 0", distance != 0);
+        assertTrue("Distance between 'abacate' and 'banana' have to be 3", distance == 4);
+    }
+
+    @Test
+    public void maxAndMinTest(){
+        assertTrue("Min val between 100, 4, 9 have to be 4", UtilsService.minimum(100, 4, 9) == 4);
+    }
 
 }
